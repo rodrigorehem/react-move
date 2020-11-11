8d75ea9217eaad20058bc6d9cc714245a4047d0e
rodrigorehem <rdrehem@tjba.jus.br>
2020-11-11 20:37:16 -0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+const Contador = () =>{
+  const [contador, setContador] = useState(0)
+  const handleContador = (sinal) => {
+    setContador(contador+sinal)
+  }
+  return <div>
+    <button type="button" onClick={() => handleContador(-1)}>-</button>
+    {contador}
+    <button type="button" onClick={() => handleContador(1)}>+</button>
+  </div>
+}
 
 function App() {
+  const [participantes, setParticipantes] = useState(['Nome1','Nome2'])
+  const [input, setInput] = useState('')
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+        {participantes.map((participante, idx)=>{
+          return <li key={idx}>{participante}</li>
+        })}
+        </ul>
+        <button type="button" onClick={handleAdicionarParticipantes}></button>
+        <frag>HELLO </frag>
+        <Contador/>
+        <ComponenteFuncional name={"Rodrigo"}> teste</ComponenteFuncional>
+        <ComponeteFuncionalComArrow name={"Rodrigo2"} idade={10}></ComponeteFuncionalComArrow>
       </header>
     </div>
   );
 }
+
+function handleAdicionarParticipantes(){
+
+}
+function ComponenteFuncional(props){
+  return <h1> Oi {props.name}</h1>
+}
+
+const ComponeteFuncionalComArrow = ({name, idade}) =>{
+  return (<div>
+    <h3>Exemplo de componente {name} e tem {idade}</h3>
+  </div>)
+}
+
 
 export default App;
